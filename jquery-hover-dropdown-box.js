a157c6f35a4148e474396adac7d23741f122f2f8
Masanori Ohgita <mp@ohgita.info>
2013-08-23 00:56:39 +0900

    Add: Action link for input element of item

--- jquery-hover-dropdown-box.js
+++ jquery-hover-dropdown-box.js
@@ -85,6 +85,13 @@
 					var id = $($(this).parents('.popup_dropdown_box')[0]).data('hoverDropdownBoxId');
 					var item_key = $(this).data('hoverDropdownBoxBoxItemKey')
 					var item_obj = hoverDropdownBoxs[id].items[item_key];
+					if( hoverDropdownBoxs[id].options.items[item_key].inputType != null
+					 && hoverDropdownBoxs[id].options.items[item_key].inputType == 'text'){
+					 	// Ignore When input form (text) is visible
+						if($(item_obj.innerInputObject).is(':visible')){
+							return true;
+						}
+					}
 					// options.onClick( item_key, item_object, dom_object )
 					(hoverDropdownBoxs[id].options.onClick)(item_key, item_obj, this);
 				});
@@ -98,6 +105,18 @@
 					// Check event source
 					if(item_obj.innerInputObject != null && $(item_obj.innerInputObject).get(0) == $(evt.target).get(0)){
 						return true;
+					}
+					if(item_obj.innerInputActionLeftObject != null && $(item_obj.innerInputActionLeftObject).get(0) == $(evt.target).get(0)){
+						return true;
+					}
+					if(item_obj.innerInputActionRightObject != null && $(item_obj.innerInputActionRightObject).get(0) == $(evt.target).get(0)){
+						return true;
+					}
+					if( hoverDropdownBoxs[id].options.items[item_key].inputType != null
+					 && hoverDropdownBoxs[id].options.items[item_key].inputType == 'text'){
+						if($(item_obj.innerInputObject).is(':visible')){
+							return true;
+						}
 					}
 					// options.onLabelClick( item_key, item_object, dom_object )
 					(hoverDropdownBoxs[id].options.onLabelClick)(item_key, item_obj, this);
@@ -276,7 +295,9 @@
 		this.key = item_key; // key or 'footer'
 		this.dom = dom_object;
 		this.labelObject = $(dom_object.children('.label'))[0];
-		this.innerInputObject = null;
+		this.innerInputObject = null; // Input form or checkbox
+		this.innerInputActionRightObject = null; // Enter link
+		this.innerInputActionLeftObject = null; // Cancel link
 	};
 
 	// Generate of input object
@@ -285,12 +306,6 @@
 
 		// Checkbox (Checkbox like div)
 		if( input_type == "checkbox" ){
-			/*var $check = jQuery('<input/>');
-			$check.attr('type', 'checkbox');
-			$check.addClass('checkbox');
-			if(item_options.inputSelected != null && item_options.inputSelected){
-				$check.prop('checked', true);
-			}*/
 			var $check = jQuery('<div/>');
 			$check.addClass('checkbox');
 			if(item_options.inputSelected != null && item_options.inputSelected){
@@ -339,15 +354,16 @@
 					item_obj = hoverDropdownBoxs[id].items[item_key];
 				}
 				if($(item_obj.innerInputObject).is(':visible')){
-					if($(evt.target)[0] != $(item_obj.innerInputObject)[0]){
-						// Cancel
-						$(item_obj.innerInputObject).hide();
-						$(item_obj.labelObject).show();
-						return;
-					}
+					return true;
 				}
 				// Check event source
 				if(item_obj.innerInputObject != null && $(item_obj.innerInputObject).get(0) == $(evt.target).get(0)){
+					return true;
+				}
+				if(item_obj.innerInputActionLeftObject != null && $(item_obj.innerInputActionLeftObject).get(0) == $(evt.target).get(0)){
+					return true;
+				}
+				if(item_obj.innerInputActionRightObject != null && $(item_obj.innerInputActionRightObject).get(0) == $(evt.target).get(0)){
 					return true;
 				}
 				// Hide the label
@@ -355,12 +371,16 @@
 				// Show the textbox, and focus to it
 				$(item_obj.innerInputObject).show();
 				$(item_obj.innerInputObject).focus();
+				// Show the action link
+				$(item_obj.innerInputActionLeftObject).show();
+				$(item_obj.innerInputActionRightObject).show();
 			});
 			$text.keypress( function(evt){
 				if(evt.which == 13){ // Enter
 					var $text = $(evt.target);
 					var id = $($(this).parents('.popup_dropdown_box')[0]).data('hoverDropdownBoxId');
-					var item_key = $($(this).parents('.list_item')[0]).data('hoverDropdownBoxBoxItemKey');				
+					var item_key = $($(this).parents('.list_item')[0]).data('hoverDropdownBoxBoxItemKey');
+					var item_obj;
 					if(item_key == 'footer'){
 						item_obj = hoverDropdownBoxs[id].footerItem;
 					} else {
@@ -372,17 +392,86 @@
 					}
 					// Hide the textbox
 					$(item_obj.innerInputObject).hide();
+					$(item_obj.innerInputActionLeftObject).hide();
+					$(item_obj.innerInputActionRightObject).hide();
 					// Show the label
 					$(item_obj.labelObject).show();
 					return false;
 				} else if(evt.keyCode == 27){ // ESC
 					// Cancel
 					$(item_obj.innerInputObject).hide();
+					$(item_obj.innerInputActionLeftObject).hide();
+					$(item_obj.innerInputActionRightObject).hide();
 					$(item_obj.labelObject).show();
 				}
 			});
+
 			this.dom.append($text);
 			this.innerInputObject = $text;
+
+			// Action link (right) - OK
+			this.generateInputActionObject_(item_options, '&gt;', 'right',
+				function(evt) {
+					var id = $($(this).parents('.popup_dropdown_box')[0]).data('hoverDropdownBoxId');
+					var item_key = $($(this).parents('.list_item')[0]).data('hoverDropdownBoxBoxItemKey');
+					var item_obj;
+					if(item_key == 'footer'){
+						item_obj = hoverDropdownBoxs[id].footerItem;
+					} else {
+						item_obj = hoverDropdownBoxs[id].items[item_key];
+					}
+					var $text = item_obj.innerInputObject;
+					if(hoverDropdownBoxs[id].options.onTextInput != null){
+						// options.onTextInput(value, item_key, item_object, dom_object)
+						(hoverDropdownBoxs[id].options.onTextInput)( $text.val(), item_key, item_obj, $text );
+					}
+					// Hide the textbox
+					$(item_obj.innerInputObject).hide();
+					$(item_obj.innerInputActionLeftObject).hide();
+					$(item_obj.innerInputActionRightObject).hide();
+					// Show the label
+					$(item_obj.labelObject).show();
+				}
+			);
+
+			// Action link (left) - Cancel
+			this.generateInputActionObject_(item_options, 'X', 'left',
+				function(evt) {
+					var id = $($(this).parents('.popup_dropdown_box')[0]).data('hoverDropdownBoxId');
+					var item_key = $($(this).parents('.list_item')[0]).data('hoverDropdownBoxBoxItemKey');
+					var item_obj;
+					if(item_key == 'footer'){
+						item_obj = hoverDropdownBoxs[id].footerItem;
+					} else {
+						item_obj = hoverDropdownBoxs[id].items[item_key];
+					}
+					// Hide the textbox
+					$(item_obj.innerInputObject).hide();
+					$(item_obj.innerInputActionLeftObject).hide();
+					$(item_obj.innerInputActionRightObject).hide();
+					// Show the label
+					$(item_obj.labelObject).show();
+				}
+			);
+		}
+	};
+
+	// Generate of action link object (for input object)
+	hoverDropdownBoxItem.prototype.generateInputActionObject_ = function( item_options, label, position, on_click ){
+		var $action = $('<span/>');
+		$action.hide();
+
+		$action.addClass('input_action');
+		$action.html(label);
+		$action.click( on_click );
+
+		this.dom.append($action);
+		if(position == 'left'){
+			$action.addClass('input_action_left');
+			this.innerInputActionLeftObject = $action;
+		} else {
+			$action.addClass('input_action_right');
+			this.innerInputActionRightObject = $action;
 		}
 	};
 
@@ -398,12 +487,12 @@
 	hoverDropdownBoxItem.prototype.checked = function( value ){
 		if(this.innerInputObject){
 			if(value != null && value == true){
-				//$(this.innerInputObject[0]).prop('checked', true);
+				this.parentObject.options.items[this.key].inputSelected = true;
 				$(this.innerInputObject[0]).data('isChecked', true);
 				// Drawing custom checkbox
 				$(this.innerInputObject[0]).addClass('checkbox_checked');
 			} else if(value != null && value == false){
-				//$(this.innerInputObject[0]).prop('checked', null);
+				this.parentObject.options.items[this.key].inputSelected = false;
 				$(this.innerInputObject[0]).data('isChecked', false);
 				// Drawing custom checkbox
 				$(this.innerInputObject[0]).removeClass('checkbox_checked');
